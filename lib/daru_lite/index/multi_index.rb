4e2432a8cd173746ca7ba8b93036a56890bc2ece
Maxime L. <lasserre.maxime@yahoo.fr>
2026-03-25 17:19:40 +0100

    fix multi_index subset

--- lib/daru_lite/index/multi_index.rb
+++ lib/daru_lite/index/multi_index.rb
@@ -169,6 +169,9 @@ module DaruLite
     def subset(*indexes)
       if indexes.first.is_a? Integer
         MultiIndex.from_tuples(indexes.map { |index| key(index) })
+      elsif include? indexes.first
+        # Same logic as in DaruLite::Index#subset
+        MultiIndex.from_tuples indexes
       else
         self[indexes].conform indexes
       end
